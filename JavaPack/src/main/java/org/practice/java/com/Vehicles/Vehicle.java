e2af7db945b09baa78e64913b6a8f8a60a6b2646
RACHIT NARANG <93432003+Rachit1602@users.noreply.github.com>
2024-12-27 15:53:34 +0530

    a new interface named vehicle is created and a class Bike is implementing it (#3)
    Co-authored-by: Rachit Narang <rachit@Rachits-MacBook-Air.local>

---
+++ JavaPack/src/main/java/org/practice/java/com/Vehicles/Vehicle.java
@@ -0,0 +1,10 @@
+package org.practice.java.com.Vehicles;
+
+public interface Vehicle {
+    int getSpeed();
+    void brakes();
+    int getGear();
+    void gearUp();
+    void gearDown();
+    void incSpeed(int inc);
+}
